fc5553e669b70ec9d38e8b002449f5dcdc58b372
Serkan Köse <sks@reddoxx.com>
2025-02-11 11:50:06 +0100

    dynamic secret

---
+++ apps/base/linkding/_init/create-secret-via-kubectl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -E
+set -o pipefail
+set -u
+
+SECRET_NAME=linkding-secrets
+USERNAME=superuser
+PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
+
+if kubectl get secret "$SECRET_NAME" > /dev/null 2>&1; then
+    echo "Secret '$SECRET_NAME' already exists. Exiting successfully."
+    exit 0
+fi
+
+
+until kubectl get secret/"$SECRET_NAME" ; do
+
+  echo "INFO: initially creating secret for LinkDing"
+
+  kubectl create secret generic "$SECRET_NAME" \
+    --from-literal="BASIC_AUTH_USERNAME"="$USERNAME"\
+    --from-literal="BASIC_AUTH_PASSWORD"="$PASSWORD" \
+    --field-manager "(basename $0)"
+
+  kubectl annotate secret "$SECRET_NAME" \
+    'homelab/managed-by'="$(basename "$0")"
+
+  sleep 1
+
+done
